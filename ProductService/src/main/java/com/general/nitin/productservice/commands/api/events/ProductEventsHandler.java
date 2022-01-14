7280eaf2678c2e3b2d27c7c7447736be029d04a8
Nitin Kumar <nitin.kumar15@sap.com>
2022-01-14 15:03:38 +0530

    feat!: exception handling on event handler There is a catch, error message is not show in the api response

--- ProductService/src/main/java/com/general/nitin/productservice/commands/api/events/ProductEventsHandler.java
+++ ProductService/src/main/java/com/general/nitin/productservice/commands/api/events/ProductEventsHandler.java
@@ -2,11 +2,14 @@ package com.general.nitin.productservice.commands.api.events;
 
 import com.general.nitin.productservice.commands.api.data.entity.Product;
 import com.general.nitin.productservice.commands.api.data.repository.ProductRepository;
+import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@ProcessingGroup("product")
 public class ProductEventsHandler {
 
     private final ProductRepository productRepository;
@@ -16,10 +19,17 @@ public class ProductEventsHandler {
     }
 
     @EventHandler
-    public void on(ProductCreatedEvent event) {
+    public void on(ProductCreatedEvent event) throws Exception {
         Product product = new Product();
         BeanUtils.copyProperties(event, product);
 
         productRepository.save(product);
+
+        throw new Exception("Exception Occurred");
+    }
+
+    @ExceptionHandler
+    public void handle(Exception exception) throws Exception {
+        throw exception;
     }
 }
